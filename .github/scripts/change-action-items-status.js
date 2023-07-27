d95fb4d0ec94c10a20ceb6cd608d92a8715ac7b7
Travis Nickels <travis.nickels@particular.net>
2023-07-27 11:16:35 -0700

    Add step to check if issue is an action item

---
+++ .github/scripts/change-action-items-status.js
@@ -0,0 +1,44 @@
+module.exports = async ({github, context}) => {
+
+  const statusFieldId = getProjectV2FieldId('Status');
+  console.log(github);
+  console.log(context);
+
+  /* async function updateStatus(projectId, itemId, fieldId, value){
+    const mutation = `mutation($projectId: ID!, $itemId: ID!, $fieldId: ID!, $value: Date!){
+      updateProjectV2ItemFieldValue(input: {
+        projectId: $projectId
+        itemId: $itemId
+        fieldId: $fieldId
+        value: {date: $value}
+      }){
+        projectV2Item {
+          id
+          fieldValueByName(name: "Title"){
+            ... on ProjectV2ItemFieldTextValue {
+              text
+            }
+          }
+        }
+      }
+    }`;
+    const variables = {
+      projectId: projectId,
+      itemId: itemId,
+      fieldId: fieldId,
+      value: value
+    };
+
+    return await github.graphql(mutation, variables);
+  } */
+
+  function getProjectV2FieldId(name) {
+    for (const field of queryResult.user.projectV2.fields.nodes){
+      if (field.name === name){
+        return field.id;
+      }
+    }
+  }
+
+  return;
+}
