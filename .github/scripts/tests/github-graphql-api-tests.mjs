528105f6d5b9a818054faa821c0dbf9bfd8ab0e1
Travis Nickels <travis.nickels@particular.net>
2023-09-13 20:26:06 -0700

    test updates

--- .github/scripts/tests/github-graphql-api-tests.mjs
+++ .github/scripts/tests/github-graphql-api-tests.mjs
@@ -1,6 +1,9 @@
-const assert = require('assert');
-const { describe } = require('node:test');
-const { Octokit } = require("@octokit/rest");
+//const assert = require('assert');
+//const { describe } = require('node:test');
+//const { Octokit } = require("@octokit/rest");
+import { describe, it } from 'mocha';
+//import assert from 'assert';
+import Octokit from '@octokit/rest'
 import * as githubGraphQLApi from "../github-graphql-api.mjs";
 
 describe('My Tests', function() {
