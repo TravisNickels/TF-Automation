a17d865dfe079369219a54e6c9efcedc7e223bdd
Travis Nickels <travis.nickels@particular.net>
2023-09-13 19:54:24 -0700

    Add mocha test

---
+++ .github/scripts/tests/github-graphql-api-tests.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const { describe } = require('node:test');
+const { Octokit } = require("@octokit/rest");
+import * as githubGraphQLApi from "../github-graphql-api.mjs";
+
+describe('My Tests', function() {
+  it('should pass this test', async function() {
+
+    const octokit = new Octokit({
+      auth: process.env.github-token,
+    });
+
+    const response = await githubGraphQLApi.getProjectV2Data(170, 'TravisNickels', octokit );
+
+    expect(response.user.projectV2.id).to.equal(170);
+  });
+});
