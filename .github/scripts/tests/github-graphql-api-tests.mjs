ac64fc429a58850773958e066c4db6628cef40b8
Travis Nickels <travis.nickels@particular.net>
2023-09-13 22:35:45 -0700

    Update auth and tokens

--- .github/scripts/tests/github-graphql-api-tests.mjs
+++ .github/scripts/tests/github-graphql-api-tests.mjs
@@ -5,15 +5,19 @@ import { describe, it } from 'mocha';
 //import assert from 'assert';
 //import { Octokit } from '@octokit/rest'
 import github from '@actions/github';
+import core from '@actions/core';
 //import { Octokit } from 'octokit'
 import * as githubGraphQLApi from "../github-graphql-api.mjs";
 
 describe('My Tests', function() {
   it('should pass this test', async function() {
 
+    console.log("TN_PAT: " + process.env.TN_PAT );
     console.log("GITHUB_TOKEN: " + process.env.GITHUB_TOKEN );
     console.log("github object: " + github );
-    const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
+
+    const myToken = core.getInput('TN_PAT')
+    const octokit = github.getOctokit(myToken);
 
     /* const octokit = new Octokit({
       auth: process.env.GITHUB_TOKEN,
