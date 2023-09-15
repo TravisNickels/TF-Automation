8421b54ccc5f9185a88036f62e84fa704eebb0a5
Travis Nickels <travis.nickels@particular.net>
2023-09-14 23:41:49 -0700

    clean up

--- .github/scripts/tests/github-graphql-api-tests.mjs
+++ .github/scripts/tests/github-graphql-api-tests.mjs
@@ -1,43 +1,25 @@
-//const assert = require('assert');
-//const { describe } = require('node:test');
-//const { Octokit } = require("@octokit/rest");
 import { describe, it, before } from 'mocha';
 import { expect } from 'chai';
 import fetchMock from 'fetch-mock';
-//import assert from 'assert';
 import { Octokit } from '@octokit/rest';
-import { graphql } from '@octokit/graphql';
+import * as githubGraphQLApi from "../github-graphql-api.mjs";
+
+//import assert from 'assert';
 //import github from '@actions/github';
-//import pkg from '@actions/github';
-//const { Github } = pkg;
 //import * as github from '@actions/github';
 //import core from '@actions/core';
-//import { Octokit } from 'octokit'
-//import { Octokit } from '@octokit/core';
 //import { createTokenAuth } from '@octokit/auth-token';
-import * as githubGraphQLApi from "../github-graphql-api.mjs";
 
 describe('My Tests', function() {
   before(() => {
-    // Set up nock to intercept Github API requests
-    /* fetchMock.post('https://api.github.com/graphql', {
-      body: {
-        data: {
-          repository: {
-            name: 'mocked-repo',
-            description: 'Mocked description',
-          },
-        },
-      headers: { 'content-type': 'application/json' },
-      }
-    }); */
-
+    // Set up Fetchmock to intercept Github API requests
     fetchMock.post('https://api.github.com/graphql', {
       body: {
         data: {
           user: {
             projectV2: {
               id: 'PVT_UyhstYisiOxQ8yTr',
+              description: 'Testing',
             }
           },
         },
@@ -49,47 +31,19 @@ describe('My Tests', function() {
 
     //const auth = createTokenAuth(process.env.TN_PAT);
     //const { token } = await auth();
-
     //const octokit = new Octokit({ auth: token });
-    //const myToken = core.getInput('TN_PAT')
     //const myToken = process.env.TN_PAT;
     //const octokit = github.getOctokit(myToken);
-
     //const octokit = new Octokit(myToken);
 
     const octokit = new Octokit({
       auth: process.env.TN_PAT,
     });
 
-    // Your GraphQL query
-    /* const query = `
-      query {
-        repository(owner: "TravisNickels", name: "TF-Automation") {
-          name
-          description
-        }
-      }
-    `; */
-
-    /* try {
-      const response = await octokit.graphql(query);
-      console.log("response.repository.name: " + response.repository.name);
-      console.log("response.repository.description: " + response.repository.description);
-      expect(response.repository.name).to.equal('mocked-repo');
-      return response.repository;
-    } catch (error) {
-      throw error;
-    } */
-
-    //const response = await graphql(query, { headers: { authorization: process.env.TN_PAT, }, }, variables);
-
-    //const response = await octokit.graphql(query, variables);
-
     const response = await githubGraphQLApi.getProjectV2Data(170, 'TravisNickels', octokit );
 
     console.log("response.user.projectV2.id: " + response.user.projectV2.id);
     expect(response.user.projectV2.id).to.equal('PVT_UyhstYisiOxQ8yTr');
-    //expect(response.user.projectV2.id).to.equal('PVT_kwHOBTAVOs4ASxQ8_asdf');
     expect(response).to.deep.equal({ user: { projectV2: { id: "PVT_UyhstYisiOxQ8yTr"}}});
   });
 });
