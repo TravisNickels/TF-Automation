c3846618c3d2d58f2ba1d3b748c67bca87830cbd
Travis Nickels <travis.nickels@particular.net>
2023-09-14 22:03:40 -0700

    updates

--- .github/scripts/tests/github-graphql-api-tests.mjs
+++ .github/scripts/tests/github-graphql-api-tests.mjs
@@ -20,7 +20,6 @@ import * as githubGraphQLApi from "../github-graphql-api.mjs";
 describe('My Tests', function() {
   before(() => {
     // Set up nock to intercept Github API requests
-    
   })
   it('should pass this test', async function() {
 
@@ -38,45 +37,15 @@ describe('My Tests', function() {
       auth: process.env.TN_PAT,
     });
 
-
-    const query = `query($owner: String!, $projectNumber: Int!){
-      user(login:$owner) {
-        projectV2(number:$projectNumber) {
-          id
-          title
-          fields(first: 100){
-            nodes {
-              ... on ProjectV2Field {
-                name
-                id
-              }
-              ... on ProjectV2SingleSelectField {
-                name
-                id
-                options{
-                  name
-                  id
-                }
-              }
-            }
-          }
-        }
-      }
-    }`;
-    const variables = {
-      owner: 'TravisNickels',
-      projectNumber: 170,
-    };
-
     nock('https://api.github.com')
       .post('/graphql') // Mock the GraphQL endpoint
-      .reply(200, { data: { user: { projectV2: { id: "PVT_UyhstYisiOxQ8yTr"}}}});
+      .reply(200, { user: { projectV2: { id: "PVT_UyhstYisiOxQ8yTr"}}});
 
-    const response = await graphql(query, { headers: { authorization: process.env.TN_PAT, }, }, variables);
+    //const response = await graphql(query, { headers: { authorization: process.env.TN_PAT, }, }, variables);
 
     //const response = await octokit.graphql(query, variables);
 
-    //const response = await githubGraphQLApi.getProjectV2Data(170, 'TravisNickels', octokit );
+    const response = await githubGraphQLApi.getProjectV2Data(170, 'TravisNickels', octokit );
 
     expect(response.user.projectV2.id).to.equal('PVT_UyhstYisiOxQ8yTr');
     //expect(response.user.projectV2.id).to.equal('PVT_kwHOBTAVOs4ASxQ8_asdf');
