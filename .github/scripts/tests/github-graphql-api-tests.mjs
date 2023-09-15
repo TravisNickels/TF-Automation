e06566e4b0c89383335ebff2f0e99a10f0435f2a
Travis Nickels <travis.nickels@particular.net>
2023-09-14 19:51:49 -0700

    Clean up

--- .github/scripts/tests/github-graphql-api-tests.mjs
+++ .github/scripts/tests/github-graphql-api-tests.mjs
@@ -8,28 +8,29 @@ import { expect } from 'chai';
 //import github from '@actions/github';
 //import pkg from '@actions/github';
 //const { Github } = pkg;
-import * as github from '@actions/github';
+//import * as github from '@actions/github';
 //import core from '@actions/core';
 //import { Octokit } from 'octokit'
 import { Octokit } from '@octokit/core';
-import { createTokenAuth } from '@octokit/auth-token';
+//import { createTokenAuth } from '@octokit/auth-token';
 import * as githubGraphQLApi from "../github-graphql-api.mjs";
 
 describe('My Tests', function() {
   it('should pass this test', async function() {
 
-    const auth = createTokenAuth(process.env.TN_PAT);
-    const { token } = await auth();
-    const octokit = new Octokit({ auth: token });
+    //const auth = createTokenAuth(process.env.TN_PAT);
+    //const { token } = await auth();
+
+    //const octokit = new Octokit({ auth: token });
     //const myToken = core.getInput('TN_PAT')
     //const myToken = process.env.TN_PAT;
     //const octokit = github.getOctokit(myToken);
 
     //const octokit = new Octokit(myToken);
 
-    /* const octokit = new Octokit({
+    const octokit = new Octokit({
       auth: process.env.GITHUB_TOKEN,
-    }); */
+    });
 
     const response = await githubGraphQLApi.getProjectV2Data(170, 'TravisNickels', octokit );
 
