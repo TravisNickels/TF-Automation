6441003dc5ee467c4283b2a582eaa57338d3705d
Travis Nickels <travis.nickels@particular.net>
2023-09-14 20:29:54 -0700

    added nock

--- .github/scripts/tests/github-graphql-api-tests.mjs
+++ .github/scripts/tests/github-graphql-api-tests.mjs
@@ -3,6 +3,7 @@
 //const { Octokit } = require("@octokit/rest");
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
+import { nock } from 'nock';
 //import assert from 'assert';
 import { Octokit } from '@octokit/rest'
 //import github from '@actions/github';
@@ -16,6 +17,12 @@ import { Octokit } from '@octokit/rest'
 import * as githubGraphQLApi from "../github-graphql-api.mjs";
 
 describe('My Tests', function() {
+  before(() => {
+    // Set up nock to intercept Github API requests
+    nock('http://api.github.com')
+      .post('graphql') // Mock the GraphQL endpoint
+      .reply(200, { data: { user: { projectV2: { id: "PVT_kwHOBTAVOs4ASxQ8_asdf"}}}})
+  })
   it('should pass this test', async function() {
 
     //const auth = createTokenAuth(process.env.TN_PAT);
@@ -34,6 +41,6 @@ describe('My Tests', function() {
 
     const response = await githubGraphQLApi.getProjectV2Data(170, 'TravisNickels', octokit );
 
-    expect(response.user.projectV2.id).to.equal('PVT_kwHOBTAVOs4ASxQ8');
+    expect(response.user.projectV2.id).to.equal('PVT_kwHOBTAVOs4ASxQ8_asdf');
   });
 });
